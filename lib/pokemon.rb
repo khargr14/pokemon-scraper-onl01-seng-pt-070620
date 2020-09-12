85738346cea3908e484785a7c6f1414b1f10db5b
Keesha Hargrove <kharg14@gmail.com>
2020-09-12 07:11:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,31 @@
 class Pokemon
-  
+  attr_accessor :name, :type, :db
+    attr_reader :id
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name 
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT * 
+            FROM pokemon
+            WHERE id = ?
+        SQL
+
+        db.execute(sql, id).map do |poke|
+            pokemon = Pokemon.new(id: poke[0], name: poke[1], type: poke[2], db: nil)
+        end.first
+    end
 end
